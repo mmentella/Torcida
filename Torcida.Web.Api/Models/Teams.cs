add973d0706dc42dd74952fdeab38c3d23f07960
Matteo Mentella <mmentella@gmail.com>
2018-02-14 17:36:11 +0100

    Nomi delle squadre italiane di Serie A

---
+++ Torcida.Web.Api/Models/Teams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Torcida.Web.Api.Models
+{
+    public static class Teams
+    {
+        public static readonly string Atalanta = nameof(Atalanta);
+        public static readonly string Benevento = nameof(Benevento);
+        public static readonly string Bologna = nameof(Bologna);
+        public static readonly string Cagliari = nameof(Cagliari);
+        public static readonly string Chievo = nameof(Chievo);
+        public static readonly string Crotone = nameof(Crotone);
+        public static readonly string Fiorentina = nameof(Fiorentina);
+        public static readonly string Genoa = nameof(Genoa);
+        public static readonly string Verona = nameof(Verona);
+        public static readonly string Inter = nameof(Inter);
+        public static readonly string Juventus = nameof(Juventus);
+        public static readonly string Lazio = nameof(Lazio);
+        public static readonly string Milan = nameof(Milan);
+        public static readonly string Napoli = nameof(Napoli);
+        public static readonly string Roma = nameof(Roma);
+        public static readonly string Sampdoria = nameof(Sampdoria);
+        public static readonly string Sassuolo = nameof(Sassuolo);
+        public static readonly string Spal = nameof(Spal);
+        public static readonly string Torino = nameof(Torino);
+        public static readonly string Udinese = nameof(Udinese);
+    }
+}
